4d0478fc6b2379a0e7836851efca06e403c6a751
Kulazar <eneanuch@gmail.com>
2020-12-13 14:50:27 +0300

    remove UI.ui, added new method of design

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5 import uic
 from random import randint
+from PyQt5 import QtCore, QtWidgets
 
 
-class Main(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(317, 379)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(100, 260, 121, 23))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 317, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Круг (не квадрат)"))
+        self.pushButton.setText(_translate("MainWindow", "Кнопка (на месте)"))
+
+
+class Main(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
 
         self.draw_circle = 0
 
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.do_circles)
 
     def do_circles(self):
@@ -40,4 +67,3 @@ if __name__ == "__main__":
     ex = Main()
     ex.show()
     exit(app.exec_())
-    # do window, if window closed, program will exit
